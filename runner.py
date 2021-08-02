500c07d005e2771984cadc2e92b6a73d964226cf
Keonyoung Shim <keonyoung.shim@navercorp.com>
2021-08-02 23:40:43 +0900

    feat: get ingame status

--- runner.py
+++ runner.py
@@ -1,31 +1,64 @@
-import requests, argparse
+import json
+import requests
+import argparse
+import datetime
 
 from config import API_KEY, RIOT_API_BASE
 
-
+SUMMONER_NAME = "/lol/summoner/v4/summoners/by-name/{summoner_name}"
 BY_SUMMONER = "/lol/spectator/v4/active-games/by-summoner/{summoner_id}"
 
+headers = {
+    "X-Riot-Token": API_KEY,
+    "Content-Type": "application/json; charset=utf-8"
+}
 
-# TODO: find game time and return the time
-def main(*args):
-    summoner_id = args["user_id"]
 
+def user_id(user_name):
+    url = RIOT_API_BASE + SUMMONER_NAME
+
+    res = requests.get(url.format(summoner_name=user_name), headers=headers)
+    res = json.loads(res.text)
+
+    return res["id"]
+
+
+def game_status(summoner_id):
     url = RIOT_API_BASE + BY_SUMMONER
-
-    headers = {
-        "X-Riot-Token": API_KEY,
-        "Content-Type": "application/json; charset=utf-8"
-    }
-
     res = requests.get(url.format(summoner_id=summoner_id), headers=headers)
+    res = json.loads(res.text)
 
-    print(res.text)
+    game_length = res["gameLength"]
+    game_start_time = res["gameStartTime"] / 1000.0
+    game_start_time_human_readable = datetime.datetime.fromtimestamp(game_start_time).strftime('%Y-%m-%d %H:%M:%S')
+
+    return game_start_time_human_readable, game_length
+
+
+def main(args):
+    user_name = args.user_name
+    try:
+        summoner_id = user_id(user_name)
+    except KeyError:
+        print("User Not found")
+        return
+
+    try:
+        start_time, elapsed_time = game_status(summoner_id)
+    except KeyError:
+        print("Game is not started yet.")
+        return
     
+    print(start_time, elapsed_time)
 
+    return elapsed_time
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("user_id")
+    parser.add_argument("user_name")
     args = parser.parse_known_args()
+    start_time = 1627912367163 / 1000.0
+    start = datetime.datetime.fromtimestamp(start_time).strftime('%Y-%m-%d %H:%M:%S')
+
     main(args[0])
